cad4584e676100f841242f4a896aa3ca14b9b563
Gaoke Wu <gaokewoo@126.com>
2014-03-26 18:54:19 +0800

    [task] Extract so create to a class and add a class to parse plugin configuration

---
+++ container/SharedObject.hh
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <dlfcn.h>
+#include <memory>
+#include "../plugin/plugin.hh"
+
+using namespace std;
+
+#ifndef __SHAREDOBJECT__
+#define __SHAREDOBJECT__
+
+typedef Plugin* CreateType(); 
+typedef void DestroyType(Plugin*); 
+
+class SharedObject
+{
+    public:
+        SharedObject(string pluginName,string soName);
+        ~SharedObject();
+        Plugin * createPlugin();
+        void destroyPlugin(Plugin * p);
+
+    private:
+        string m_pluginName;
+        string m_soName;
+        void * handler;
+        CreateType * createRef;
+        DestroyType * destroyRef;
+        Plugin * plugin;
+        static const string CLASS_LENGTH;
+        static const string CLASS_NAME;
+        static const string CREATE_PLUGIN_FUNC;
+        static const string DESTROY_PLUGIN_FUNC;
+
+};
+
+#endif
+
---
+++ controller/PluginParser.hh
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "../tool/tinyxml2/tinyxml2.h"
+
+#ifndef __PLUGINPARSER__
+#define __PLUGINPARSER__
+
+using namespace std;
+using namespace tinyxml2;
+
+class PluginParser
+{
+    public:
+        PluginParser(string xml_file);
+        void printXML();
+
+    private:
+        string m_xml_file;
+
+};
+
+#endif
