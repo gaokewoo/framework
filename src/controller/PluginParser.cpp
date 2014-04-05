751c612d63deef77d4011d1bb1489b47255f0e6a
Gaoke Wu <gaokewoo@126.com>
2014-04-06 00:12:27 +0800

    [task] Add glog and gmock to framework

--- src/controller/PluginParser.cpp
+++ src/controller/PluginParser.cpp
@@ -3,20 +3,44 @@
 PluginParser::PluginParser(string xml_file)
 {
     m_xml_file = xml_file;
+    doc.LoadFile(m_xml_file.c_str());
 }
 
 void PluginParser::printXML()
 {
-    XMLDocument doc;
-    doc.LoadFile(m_xml_file.c_str());
-
     doc.Print();
+}
+
+vector<PluginInfo> PluginParser::getPluginInfo()
+{
+    vector<PluginInfo> v_pluginInfo;
+
+    XMLElement *plugins=doc.RootElement();  
+    XMLElement *plugin=plugins->FirstChildElement("plugin");
+    while(plugin)
+    {
+        PluginInfo pluginInfo;
+        pluginInfo.m_pluginName=plugin->FirstChildElement("pluginName")->GetText();
+        pluginInfo.m_soName=plugin->FirstChildElement("library")->GetText();
+        v_pluginInfo.push_back(pluginInfo);
+
+        plugin=plugin->NextSiblingElement();
+    }
+
+    return v_pluginInfo;
 }
 
 int main()
 {
-    PluginParser parser("../config/plugin.xml");
-    parser.printXML();
+    PluginParser parser("../../config/plugin.xml");
+    //parser.printXML();
+    vector<PluginInfo> v_pluginInfo=parser.getPluginInfo();
+
+    vector<PluginInfo>::iterator it;
+    for(it=v_pluginInfo.begin();it!=v_pluginInfo.end();++it)
+    {
+        cout<<it->m_pluginName<<":"<<it->m_soName<<endl;
+    }
 
     return 0;
 }
--- src/controller/PluginParser.h
+++ src/controller/PluginParser.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "tinyxml2.h"
 
 #ifndef __PLUGINPARSER__
@@ -7,14 +8,23 @@
 using namespace std;
 using namespace tinyxml2;
 
+class PluginInfo
+{
+    public:
+        string m_pluginName;
+        string m_soName;
+};
+
 class PluginParser
 {
     public:
         PluginParser(string xml_file);
         void printXML();
+        vector<PluginInfo> getPluginInfo();
 
     private:
         string m_xml_file;
+        XMLDocument doc;
 
 };
 
